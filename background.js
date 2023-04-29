148d61ba6b2f3edb9edee0ea206af2470485a11f
Daniel Karpierz <dankarp1999@gmail.com>
2023-04-29 14:36:30 -0400

    Updates

--- background.js
+++ background.js
@@ -2,14 +2,17 @@ chrome.runtime.onInstalled.addListener(() => {
 	console.log('Extension installed!');
 });
 
-chrome.action.onClicked.addListener((tab) => {
-	chrome.scripting.executeScript({
-		target: { tabId: tab.id },
-		func: () => {
-			const font = prompt('Enter a font name:', 'Open Sans');
-			if (font) {
-				document.body.style.fontFamily = font;
-			}
-		},
-	});
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+	if (message.action === 'change-font') {
+		const font = message.font || 'Open Sans';
+		chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+			chrome.tabs.executeScript(
+				tabs[0].id,
+				{ code: `document.body.style.fontFamily = '${font}';` },
+				() => {
+					console.log(`Font set to ${font}`);
+				}
+			);
+		});
+	}
 });
--- popup.js
+++ popup.js
@@ -1,35 +1,13 @@
-// Retrieve the saved font from storage and set it on the select element
-chrome.storage.local.get('font', function (data) {
-	if (data.font) {
-		const selectElement = document.getElementById('font-select');
-		selectElement.value = data.font;
-	}
-});
-
-// Get the select element and listen for changes
-chrome.storage.local.get('font', function (data) {
-	const selectElement = document.getElementById('font-select');
-	selectElement.addEventListener('change', function () {
-		// Get the selected font and save it to storage
-		console.log('select test');
-		const selectedFont = selectElement.value;
-		chrome.storage.local.set({ font: selectedFont }, function () {
-			console.log(`Selected font set to ${selectedFont}`);
-		});
+const fontSelect = document.getElementById('font-select');
+fontSelect.addEventListener('change', () => {
+	const font = fontSelect.value;
+	chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+		chrome.tabs.sendMessage(
+			tabs[0].id,
+			{ action: 'change-font', font: font },
+			() => {
+				console.log(`Font set to ${font}`);
+			}
+		);
 	});
 });
-
-// Add a click listener to the extension icon
-// chrome.browserAction.onClicked.addListener(function (tab) {
-// 	// Inject the content script into the current tab
-// 	chrome.scripting.executeScript({
-// 		target: { tabId: tab.id },
-// 		files: ['content.js'],
-// 	});
-// });
-
-function sendMessage(message) {
-	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-		chrome.runtime.sendMessage({ action: 'change-font', font: font });
-	});
-}
